56f179a2d6638e6355f9c949ac857bad0fedb24f
abdullahehab254037 <abdullahehab173@gmail.com>
2026-09-07 15:52:40 +0300

    question3

--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -30,6 +30,19 @@
             //sizelabel = pages > 300 ? "long book" : "short book";
             //Console.WriteLine(sizelabel);
             #endregion
+
+            #region question3
+            //Given string[] books = { "Clean Code", "The Pragmatic Programmer", "Refactoring" };,
+            //use a for loop to print each book with its position number, like 1. Clean Code.
+            //string[] books = { "Clean Code", "The Pragmatic Programmer", "Refactoring" };
+            //for (int i = 0; i < books.Length; i++)
+            //{
+            //     Console.WriteLine((i+1)+"."+books[i]);
+            //   
+            //}
+
+            #endregion
+
         }
     }
 }
