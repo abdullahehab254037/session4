2ab178737b9676573299aa4a98eae8beaf2dbd57
abdullahehab254037 <abdullahehab173@gmail.com>
2026-09-07 15:38:33 +0300

    question1

--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            
+            #region question1
+            //Given string title = "Refactoring";, write a switch statement that prints "Great choice!" if the title is "Clean Code",
+            //"Nice pick!" if it's "Refactoring", and "Never heard of it" for anything else.
+            //string title = "Refactoring";
+            //switch (title)
+            //{
+            //    case "Clean Code":
+            //        Console.WriteLine("Great choice!");
+            //        break;
+            //    case "Refactoring":
+            //        Console.WriteLine("nice pick!");
+            //        break;
+            //        default: Console.WriteLine("Never heard of it");
+            //        break;
+            //}
+
+            #endregion
         }
     }
 }
