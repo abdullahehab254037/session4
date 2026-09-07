9506e69be20db61b87dbc06510be5148034c4291
abdullahehab254037 <abdullahehab173@gmail.com>
2026-09-07 16:08:10 +0300

    question7

--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -75,6 +75,18 @@
             //}
             #endregion
 
+            #region question7
+            //Using the same books array, loop through it and print each
+            //title, but stop completely (break) once you reach "Refactoring". 
+            //string[] books = { "Clean Code", "The Pragmatic Programmer", "Refactoring" };
+            //for (int i = 0; i < books.Length; i++)
+            //{
+            //    if (books[i]== "Refactoring") { break; }
+            //     Console.WriteLine((i+1)+"."+books[i]);
+            //   
+            //}
+            #endregion
+
         }
     }
 }
